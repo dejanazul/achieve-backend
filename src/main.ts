b33786f32d07da75222b723da90c1ff792d1b75f
dejanazul <dejanazoel@gmail.com>
2025-10-29 22:17:07 +0700

    update CORS 3000

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:8080'],
+    origin: [
+      process.env.FRONTEND_URL,
+      'http://localhost:8080',
+      'http://localhost:3000',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
